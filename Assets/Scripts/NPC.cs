11dfd2d1ff5bbbf8162027cb51f14a522c32c9de
Beytullah Çakır <132066307+beytullah-cakir@users.noreply.github.com>
2025-02-27 04:43:33 +0300

    .

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using DialogueEditor;
+
 public class NPC : MonoBehaviour
 {
     public NPCConversation conversation;
+
     private void Start()
     {
+        ConversationManager.OnConversationEnded += OnConversationEnd;
         ConversationManager.Instance.StartConversation(conversation);
     }
-   
+
+    private void OnConversationEnd()
+    {
+        // Yeni sahneye geç
+        SceneManager.LoadScene("Game");
+        
+        // Olayı temizle (tekrar çağrılmasını önlemek için)
+        ConversationManager.OnConversationEnded -= OnConversationEnd;
+    }
 }
